a1e36f4cc563eb7fd11671e409d6276a84c3585c
salma3rafa <103216606+salma3rafa@users.noreply.github.com>
2024-07-31 21:25:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ def predict():
     prediction = model.predict(input_features)
     return jsonify({'prediction': prediction.tolist()})
 if __name__ == '__main__':
-    app.run(port=5001,debug=True)
+    app.run(host="0.0.0.0"port=5001,debug=True)
